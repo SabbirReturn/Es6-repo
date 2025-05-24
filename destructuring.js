8f70fe5caab687e47930d7716a151105e5f56c84
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-05-24 17:34:00 +0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,8 @@
+let {name,...other} ={name:'Sabbir', Age:25, Class:'3rd year', profession:'Student'}
+
+// console.log(name);
+// console.log(other);
+
+let [first, second,...remaining] = [11,23,1344,414,3524]
+
+console.log(remaining);
