561d9f44a9438218053a82a3e7806021094f3687
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-05-29 23:48:48 +0600

    again

--- practice.js
+++ practice.js
@@ -21,4 +21,58 @@ let data = {
     }
 }
 
-console.log(data.Sophia.study[1].secondary[1].location)
+// console.log(data.Sophia.study[1].secondary[1].location)
+
+
+// let student = {
+//     2222:{
+//         name:'Jack',
+//         section:'c',
+//         class:'IX',
+//         address:{
+//             'building no':12,
+//             'street': 'St jonson',
+//             'city':'Petersburg',
+//             'country':'UK'
+//         }
+//     },
+//     3333:{
+//         name:'Herry',
+//         section:'D',
+//         class:'X',
+//         address:{
+//             'bulding no': 85,
+//             'street': 'DC road',
+//             'City': ' kachukhet',
+//             'Country': 'Bangladesh'
+//         }
+//    }
+// }
+// // console.log(student['2222'].address['city']);
+// console.log(student['3333'].name)
+let student = {
+    2222:{
+        name:'Jack',
+        section:'c',
+        class:'IX',
+        address:{
+            'building no':12,
+            'street': 'St jonson',
+            'city':'Petersburg',
+            'country':'UK'
+        }
+    },
+    3333:{
+        name:'Herry',
+        section:'D',
+        class:'X',
+        address:{
+            'bulding no': 85,
+            'street': 'DC road',
+            'City': ' kachukhet',
+            'Country': 'Bangladesh'
+        }
+   }
+}
+// console.log(student['2222'].address['city']);
+console.log(student['3333'].name)
